1633b6578c166ca6f6995b5b4b02b3c312ad3800
lansolo7905 <fkshtm43@gmail.com>
2026-03-25 22:23:39 +0900

    유닛 수정 및 이펙트 추가

---
+++ Assets/Scripts/Unit/UnitData.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+[CreateAssetMenu(menuName =  "Unit/Unit Data")]
+public class UnitData : ScriptableObject
+{
+    public string UnitName;
+    [TextArea]
+    public string UnitDescription;
+
+    public ParticleSystem muzzleFlash;
+    public ParticleSystem hitEffect;
+    public GameObject bulletLine;
+
+    public float MaxHealth;
+    public float Damage;
+    public float AttackRate;
+    public float AttackRange;
+}
